310d1a57a22744727e19456d7ba02188ccf66415
zhaoyan <zhaoyan.x_gds@scientecpeople.com>
2024-07-17 15:25:37 +0800

    Add firebase admin credentials

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
 import express, { Request, Response } from "express";
-import { initializeApp } from "firebase-admin/app";
+import admin from "firebase-admin";
 import { appCheckVerification } from "./middlewares/app-check-verification";
+import "dotenv/config";
+import * as https from "https";
 
 const app = express();
 const PORT = process.env.PORT || 2000;
-const firebaseApp = initializeApp();
+
+const agent = new https.Agent({
+  keepAlive: true,
+});
+admin.initializeApp({
+  credential: admin.credential.cert(
+    JSON.parse(process.env.FIREBASE_ADMIN_PRIVATE_KEY ?? "")
+  ),
+  httpAgent: agent,
+});
 
 app.get("/", (req: Request, res: Response) => {
+  console.log(">>>>", JSON.parse(process.env.FIREBASE_ADMIN_PRIVATE_KEY ?? ""));
+
   res.send("Appcheck Hello World from Express and TypeScript!");
 });
 
